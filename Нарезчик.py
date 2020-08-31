0946e150fa3c32fa3c09677c7fcecee9d00f4ba1
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-08-31 10:04:18 +0300

    Update Нарезчик.py

--- Нарезчик.py
+++ Нарезчик.py
@@ -1,6 +1,6 @@
-from pydub import AudioSegment
-song = AudioSegment.from_mp3("01.mp3")
-first_10_seconds = song[:10000]
-last_5_seconds = song[-5000:]
-целая = first_10_seconds + last_5_seconds
-целая.export("mashup.mp3", format="mp3")
+from pydub import AudioSegment # подключаем библиотеку для работы с mp3
+song = AudioSegment.from_mp3("01.mp3") # подгружаем mp3 файл
+first_10_seconds = song[:10000] # вырезаем первые 10 сек
+last_5_seconds = song[-5000:] # вырезаем последние 5 сек
+целая = first_10_seconds + last_5_seconds # объединяем обрезки
+целая.export("mashup.mp3", format="mp3") # записываем новый mp3
