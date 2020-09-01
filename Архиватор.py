da114b2d971a2a37c5f0ba6aae129289e02f7232
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-09-01 23:19:48 +0300

    Update Архиватор.py

--- Архиватор.py
+++ Архиватор.py
@@ -1,8 +1,17 @@
 from pyunpack import Archive
 import json
 import os
+import glob
+from pathlib import Path
+архивы = []
+for n in Path(".").glob('*.rar'):
+	архивы.append(n.name) #добавляем имя пути
+for n in Path(".").glob('*.7z'):
+	архивы.append(n.name) #добавляем имя пути
+for n in Path(".").glob('*.zip'):
+	архивы.append(n.name) #добавляем имя пути
 
-Проиндексировать файлы в папке с закаченными архивами
-для и в проиндексированной папке:
-  Archive("Test.7z").extractall(".") # разархивировать
-  os.remove("Test.7z") # удалить архив
+print(архивы)
+for i in архивы:
+	Archive(i).extractall("База") # разархивировать в папку база
+	os.remove(i) # удалить архив
