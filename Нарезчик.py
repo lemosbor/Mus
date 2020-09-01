97e00b84638b2b699aef3b4128a182c1bea2b1f8
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-09-02 00:35:10 +0300

    Add files via upload

--- Нарезчик.py
+++ Нарезчик.py
@@ -5,24 +5,22 @@ import os
 import glob
 from pathlib import Path
 #for root, dirs, files in os.walk("."):  
+папки = []
 for root, dirs, files in os.walk("."):  
-    #for filename in files:
-    print(dirs, files)
+	папки = папки + dirs
 
-for root, dirs in os.walk("."):  
-  альбомы = dirs
-  
-for n in альбомы:
-  песни = []
-  демо = ""
-  демо = AudioSegment.from_mp3(n+песни[0])[:5000] #первые пять секунд от первой песни
-  for i in Path('/путь альбома').glob('*.mp3'):
-    песня = AudioSegment.from_mp3(n+"/"+i) # подгружаем mp3 файл
-    обрезок = песня[63000:70000] + песня[95000:101000]
-    демо = демо + обрезок
-    # демо = демо.append(обрезок, crossfade=1500) # с плавным переходом
-  #демо = демо.fade_in(2000).fade_out(3000) # плавное начало и конец
-  демо.export(n+"демо.mp3", format="mp3", tags={'artist': n})
-  play(демо)
-
-# https://github.com/jiaaro/pydub
+#for root, dirs in os.walk("."):
+#  альбомы = root
+print(папки)
+for i in папки:
+	демо = 0
+	for n in Path(i).glob('*.mp3'):
+		песня = AudioSegment.from_mp3(n) # подгружаем mp3 файл
+		#обрезок = 0
+		обрезок = песня[63000:70000].fade_in(1000).fade_out(500) + песня[95000:101000].fade_in(500).fade_out(1500)
+		#обрезок=обрезок.append(песня[63000:70000] , crossfade=1500)
+		#обрезок=обрезок.append(песня[95000:101000] , crossfade=1500)
+		демо = демо + обрезок
+    	# демо = демо.append(обрезок, crossfade=1500) # с плавным переходом
+  		#демо = демо.fade_in(2000).fade_out(3000) # плавное начало и конец
+	демо.export(i+"демо.mp3", format="mp3", tags={'artist': i})
