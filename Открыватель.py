c717d3f2a7f80d9e1ff9d9e05b55d601cb317c58
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-09-02 11:09:28 +0300

    Update Открыватель.py

--- Открыватель.py
+++ Открыватель.py
@@ -1,10 +1,12 @@
 import webbrowser # библиотека работы с ссылками
 import json
+import time
 
 база = json.loads(open ('музыка.json', "r", encoding='utf-8').read()) # открываем базу
 for i in база: # для записи в массиве записей
-	if "ссылка" in i and "скачен" not in i: # если в записи есть ссылка, но нет индекса "скачен"
+	if "ссылка" in i and "скачен" not in i: # если в записи есть ссылка, но нет индекса «скачен»
 		webbrowser.open(i['ссылка']) # открыть ссылку
+		time.sleep(0.8)
 		#ПОСПАТЬ
 		i.update({'скачен' : 1}) # записать индекс "скачен"
 	with open("музыка.json", "w", encoding='utf-8') as i: i.write(json.dumps(база, ensure_ascii = False)) # записываем базу
