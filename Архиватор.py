44e680aff17eddd82d02a7b3cd6b7d00907896d4
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-09-09 08:13:45 +0300

    Add files via upload

--- Архиватор.py
+++ Архиватор.py
@@ -1,16 +1,27 @@
 from pyunpack import Archive # библиотека работы с архивами. Для установки библиотеки необходимо pip install patool, pip install pyunpack (из папки с 7Z) и прописать папку с 7Z в пути Path
 import json
-import os
-import glob # библиотека работы с расширениями
 from pathlib import Path # библиотека для работы с папками
+from pydub import AudioSegment # подключаем библиотеку для работы с mp3
+from pydub.playback import play # подключаем библиотеку для воспроизведения mp3
+
 архивы = [] # создаем массив архивов
-for n in Path(".").glob('*.rar'): # для файлов с расширением *.rar
-	архивы.append(n.name) #добавляем имя пути
-for n in Path(".").glob('*.7z'): # для файлов с расширением *.7z
-	архивы.append(n.name) #добавляем имя пути
-for n in Path(".").glob('*.zip'): # для файлов с расширением *.zip
-	архивы.append(n.name) #добавляем имя пути
-print("Кол-во архивов:", архивы)
+for n in Path("/Users/Илья/Downloads").glob('*.rar'): # для файлов с расширением *.rar
+	архивы.append(str(n)) #добавляем имя пути
+for n in Path("/Users/Илья/Downloads").glob('*.7z'): # для файлов с расширением
+	архивы.append(str(n)) #добавляем имя пути
+for n in Path("/Users/Илья/Downloads").glob('*.zip'): # для файлов с расширением 
+	архивы.append(str(n)) #добавляем имя пути
+print("Кол-во архивов:", len(архивы))
 for i in архивы: # для каждого архива из массива архивов
-	Archive(i).extractall("База") # разархивировать в папку "База"
+	Archive(i).extractall("/Users/Илья/Music/Приёмка") # разархивировать в папку "База"
 	os.remove(i) # удалить архив
+	
+# НАРЕЗЧИК
+папки = [] # создаём массив директорий с музыкой
+for папка in Path('/Users/Илья/Music/Приёмка').iterdir():
+	демо = 0 #создаём демо-трек
+	for n in Path(папка).glob('*.mp3'): # в каждом файле с расширением mp3 рассматриваемой директории
+		песня = AudioSegment.from_mp3(n) # подгружаем файл
+		обрезок = песня[63000:70000].fade_in(1000).fade_out(500) + песня[95000:102000].fade_in(500).fade_out(1500) # создаем обрезок песни с 1:03—1:10 и 1:35—1:42 с затуханием
+		демо = демо + обрезок # формируем демо-трек
+	демо.export("/Users/Илья/Music/"+(папка.name)+".mp3", format="mp3", tags={'artist': папка.name}) # записываем демо-трек
