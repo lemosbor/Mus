4a3849cb9a8596a207dd2746bb03912f0755c969
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-09-18 00:15:31 +0300

    Update плеер.py

--- плеер.py
+++ плеер.py
@@ -4,21 +4,28 @@ import requests
 from io import BytesIO
 from itertools import cycle
 import tkinter as tk # Python3
+from pygame import mixer 
 
 инфо = [1, 2, 3, 4, 5]
+музыка = ["/01.mp3","/01.mp3","/01.mp3","/01.mp3","/01.mp3"]
 
 class App(tk.Tk): #'''Tk window/label adjusts to size of image'''
     def __init__(self, инфо): # the root will be self
         tk.Tk.__init__(self)# allows repeat cycling through the pictures # store as (img_object, img_name) tuple
-		self.инфоцикл = cycle(info for info in инфо) #выгружаем информацию из списка информации
+        self.инфоцикл = cycle(info for info in инфо) #выгружаем информацию из списка информации
+        self.музыкацикл = cycle(ino for ino in музыка)
         self.текстовка = tk.Label(self) #задаём область для текста
         self.текстовка.pack()
     def перебор(self): # собственно перебор фоторафий
-        self.инфо= next(self.инфоцикл) #отображаемая информация
+        self.инфо= next(self.инфоцикл)
+        self.музыка= next(self.музыкацикл) #отображаемая информация
         self.title(self.инфо) #область айди (титул)
         self.текстовка.config(text=self.инфо) #область текстовки с информцией
         self.bind('<Left>', self.ккласс0) # если жмём кнопку 0, то вызываем команду класс0
         self.bind('<Right>', self.ккласс1) # если жмём кнопку +, то вызываем команду класс0
+        mixer.init()
+        mixer.music.load(self.музыка) # загружаем файл
+        mixer.music.play() # запускаем файл
         #self.bind('<space>', self.на_доп) # если жмём кнопку вправо, то вызываем команду на_доп
     def пуск(self): #запускаем панель
         self.mainloop()
@@ -31,4 +38,3 @@ class App(tk.Tk): #'''Tk window/label adjusts to size of image'''
 app = App(инфо)
 app.перебор()
 app.пуск() # запускаем приложение
-#пав
