e9adfcf57adf25ae953c8d54cd6b62176f3ec995
lemosbor <68635720+lemosbor@users.noreply.github.com>
2020-10-07 11:54:16 +0300

    Update Парсер.py

--- Парсер.py
+++ Парсер.py
@@ -1,23 +1,28 @@
 from bs4 import BeautifulSoup # импорт класса BeautifulSoup из модуля bs4
-import requests as req # модуль для веб-запросов
+import requests # модуль для веб-запросов
 import json # модуль для работы с json
 import re # модуль для работы с текстом
 import webbrowser # модуль для работы с ссылками
 import time # модуль для работы со временем
 import mouse # модуль для имитации мышки
+from fp.fp import FreeProxy # модуль выгрузки прокси https://pypi.org/project/free-proxy/
 # ПАРСИНГ ЗАПИСЕЙ
 конец=0 # индикатор окончания просмотра страниц
-итерация =1 # первая страница
-while конец==0: # цикл перебора страниц
-	if итерация ==1: адрес ="https://funkysouls.org/music/index.html" # для первой страницы		
+итерация=1 # первая страница
+while конец == 0: # цикл перебора страниц
+	if итерация == 1: адрес ="https://funkysouls.org/music/index.html" # для первой страницы		
 	else: адрес =("https://funkysouls.org/music/page/"+str(итерация)+".html") # для последующих		
 	база = json.loads(open ('музыка.json', "r", encoding='utf-8').read()) # открываем базу
 	альбомы = [i['а'] for i in база] # создаем перечень существующих записей (альбомов)
 	предбаза = [] # создаем пустую предбазу
-	про="95.174.67.50" # IP прокси https://free-proxy-list.net/
-	кси="18080" # порт прокси
-	прокси = { 'http': "http://"+про+":"+кси, 'https': "http://"+про+":"+кси} # задаем прокси (для заблокированных сайтов)
-	запрос = req.get(адрес, proxies=прокси) # делаем запрос к сайту, используя прокси
+	
+	# для запросов к заблокированным сайтам
+	#про="95.174.67.50" # IP прокси https://free-proxy-list.net/
+	#кси="18080" # порт прокси
+	#прокси = { 'http': "http://"+про+":"+кси, 'https': "http://"+про+":"+кси} # задаем прокси (для заблокированных сайтов)
+	прокси = FreeProxy().get()
+	
+	запрос = requests.get(адрес, proxies=прокси) # делаем запрос к сайту, используя прокси
 	запрос.encoding = 'utf-8' #перекодируем результат запроса
 	суп = BeautifulSoup(запрос.text,  'lxml') # создание объекта BeautifulSoup (суп) и передача его конструктору. Вторая опция уточняет объект парсинга.
 	массив = суп.find_all("h2") # из супа формируем массив по тегу h2
@@ -37,22 +42,27 @@ while конец==0: # цикл перебора страниц
 for i in база: # прогоняем по всем записям
 	if "ссылка" not in i: # если нет ссылки на файл-архив
 		адрес = i['ф'] # адрес — ссылка на форум
-		print(адрес)
+		назв = i['а'] # название альбома
+		print(назв, адрес)
 		номер = "p"+(адрес.split('p/')[1]) # номер сообщения на форуме (индекс «р» + часть адреса. разделение по индексу p/)
-		запрос = req.get(адрес, proxies=прокси) # делаем запрос к адресу, используя прокси
+		запрос = requests.get(адрес, proxies=прокси) # делаем запрос к адресу, используя прокси
 		запрос.encoding = 'utf-8' #перекодируем результат запроса
-		суп = BeautifulSoup(запрос.text,  'lxml') # создание объекта BeautifulSoup (суп) и передача его конструктору
-		
+		суп = BeautifulSoup(запрос.text,  'lxml') # создание объекта BeautifulSoup (суп) и передача его конструктору		
 		сообщение = суп.find("div", id=номер) # находим код с тегом div и id номера сообщения
 		ссылка = сообщение.find('div', class_='text').find_all(string=re.compile('zippyshare.com')) # в сообщении находим тег div с классом text, а в нём находим ссылку по ключевому слову
+		print(ссылка)
 		for txt in ссылка:
 			i.update({'ссылка' : " ".join(txt.split())}) # записываем ссылку
-		if not "ссылка" in i: # если ссылка не нашлась, то находим по-другому
-			ссылка = суп.find_all(string=re.compile('zippyshare.com')) # находим ссылку во всём супе в обратном порядке #'https://forum.funkysouls.org/go.php?https://www117.zippyshare.com/v/Epi3DSTq/file.html'
+		if not "ссылка" in i: # если ссылка не нашлась, то находим по-другому			
+			позиция1=[суп.rfind("zippyshare.com")][0]-7 # позиция словосочетания в супе - 7 знаков
+			подсуп = суп[позиция1:]
+			позиция2=[подсуп.rfind("html")][0]+4 # позиция словосочетания в подсупе + 4 знака			
+			ссылка = подсуп[:позиция2]
+			#ссылка = суп.find_all(string=re.compile('zippyshare.com')) # находим ссылку во всём супе в обратном порядке #'https://forum.funkysouls.org/go.php?https://www117.zippyshare.com/v/Epi3DSTq/file.html'
 			print(ссылка)
-			for txt in ссылка:
-				i.update({'ссылка' : "".join(txt.split("//")[-1])}) # записываем ссылку
-				print(i['ссылка'])
+			#for txt in ссылка:
+			i.update({'ссылка' : ссылка}) # записываем ссылку
+			#print(i['ссылка'])
 		i.update({'жанр' : [] }) # записываем список жанров
 		for жанр in суп.find_all("a", class_="ftag"): # находим все жанры
 		    i["жанр"].append(жанр.text) # записываем все жанры в список жанров
@@ -68,5 +78,6 @@ for i in база: # для записи в массиве записей
 		mouse.move(900,  300, absolute=True, duration=0.2) # перевести указатель мыши в позицию 900, 300
 		mouse.click('left') # левый клик мыши
 		i.update({'скачен' : 1}) # записать индекс "скачен"
+		print(i['ф'])
 	with open("музыка.json", "w", encoding='utf-8') as i: i.write(json.dumps(база, ensure_ascii = False)) # записываем базу
 #<div class="body">	<div class="text"> <b><span style='font-size:14pt;line-height:100%'>Jon Gurd</span></b><br><br><a href='https://funkyimg.com/view/36ZUP' target='_blank'><img src='https://funkyimg.com/i/36ZUP.jpeg' alt='картинка, оставленная пользователем'></a><br><br><a href='https://forum.funkysouls.org/go.php?https://jongurd.bandcamp.com/' target='_blank'>bandcamp</a><br><br><b>Jon Gurd – Lion (2020)</b><br><br><a href='https://funkyimg.com/view/36ZUU' target='_blank'><img src='https://funkyimg.com/i/36ZUU.jpg' alt='картинка, оставленная пользователем'></a><br><br>CD1:<br>01 – Love<br>02 – Together<br>03 – Goodbye<br>04 – Lion<br>05 – Star<br>06 – See It<br>07 – District<br>08 – Endless<br>09 – You Are<br>10 – The Dream<br><br>CD2:<br>01 – Together<br>02 – Lion (Edit)<br>03 – The Dream (Edit)<br>04 – District (Edit)<br><br><a href='https://forum.funkysouls.org/go.php?https://www117.zippyshare.com/v/Epi3DSTq/file.html' target='_blank'>MP3</a>   175MB<br><br><a href='https://forum.funkysouls.org/go.php?https://www117.zippyshare.com/v/9Y0CAwcu/file.html' target='_blank'>FLAC</a>   440MB  </div>
